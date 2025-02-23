8e23dfdd76a8611b5d585487a703f9ba56fbe975
Caleb Emerson <caleb.emerson317@gmail.com>
2025-02-23 05:37:04 -0500

    Implement Pack functions

--- Pack.cpp
+++ Pack.cpp
@@ -10,8 +10,12 @@
 // NOTE: The standard order is the same as that in pack.in.
 // NOTE: Do NOT use pack.in in your implementation of this function
 // NOTE: The pack is initially full, with no cards dealt.
-Pack::Pack() {
-  assert(false);
+Pack::Pack() 
+  : next(0) {
+  for (int i = 0; i < PACK_SIZE; ++i) {
+    cards.at(i) = Card(static_cast<Rank>((i % 6) + 7),
+                  static_cast<Suit>(i / 6));
+  }
 }
 
 // REQUIRES: pack_input contains a representation of a Pack in the format
@@ -19,29 +23,55 @@ Pack::Pack() {
 // MODIFIES: pack_input
 // EFFECTS: Initializes Pack by reading from pack_input.
 // NOTE: The pack is initially full, with no cards dealt.
-Pack::Pack(std::istream& pack_input) {
-  assert(false);
+Pack::Pack(std::istream& pack_input)
+  : next(0) {
+  for ( ; !empty(); ++next) {
+    pack_input >> cards.at(next);
+  }
+
+  reset();
 }
 
 // REQUIRES: cards remain in the Pack
 // EFFECTS: Returns the next card in the pack and increments the next index
 Card Pack::deal_one() {
-  assert(false);
+  return cards.at(next++);
 }
 
 // EFFECTS: Resets next index to first card in the Pack
 void Pack::reset() {
-  assert(false);
-  assert(next); // DELETEME: avoid error "private field is not used"
+  next = 0;
 }
 
 // EFFECTS: Shuffles the Pack and resets the next index. This performs an in
 //          shuffle seven times. See https://en.wikipedia.org/wiki/In_shuffle.
 void Pack::shuffle() {
-  assert(false);
+  // In-Shuffle 7 times
+  for (int shuffle_num = 0; shuffle_num < 7; ++shuffle_num) {
+    // Arrange the top half of the pack into its own array
+    std::array<Card, PACK_SIZE / 2> top_half;
+    for (int i = 0; i < PACK_SIZE / 2; ++i) {
+      top_half.at(i) = cards.at(i);
+    }
+
+    // Arrange the bottom hald of the pack into its own array
+    std::array<Card, PACK_SIZE / 2> bottom_half;
+    for (int i = 0; i < PACK_SIZE / 2; ++i) {
+      bottom_half.at(i) = cards.at((PACK_SIZE / 2) + i);
+    }
+
+    // Reconstruct original Pack
+    for (int i = 0; i < PACK_SIZE; ++i) {
+      if (i % 2 == 0) {
+        cards.at(i) == bottom_half.at(i / 2);
+      } else {
+        cards.at(i) == top_half.at((i - 1) / 2);
+      }
+    }
+  }
 }
 
 // EFFECTS: returns true if there are no more cards left in the pack
 bool Pack::empty() const {
-  assert(false);
+  return next == PACK_SIZE;
 }
